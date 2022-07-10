d6ff75bd057f5fdc6de06268c09b00b073696d83
kashiash <kashiash@gmail.com>
2022-07-10 09:29:30 +0200

    Initial Commit

---
+++ UnitConverter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UnitConverter
+//
+//  Created by Jacek Placek on 10/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
