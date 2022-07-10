4be0ec395b0bc473892bb4ff597a534c7d1f38a8
kashiash <kashiash@gmail.com>
2022-07-10 12:28:02 +0200

    ciemne tło i więcej rodzajów konwersji

--- UnitConverter/ContentView.swift
+++ UnitConverter/ContentView.swift
@@ -9,10 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var input = 100.0
-    @State private var inputUnit = UnitLength.meters
-    @State private var outputUnit = UnitLength.kilometers
+    @State private var inputUnit: Dimension = UnitLength.meters
+    @State private var outputUnit: Dimension = UnitLength.kilometers
+
+    @State var selectedUnits = 0
+    @FocusState private var inputIsFocused: Bool
     
-    let units: [UnitLength] = [.nanometers,.kilometers,.meters,.centimeters,.decameters,.nauticalMiles,.feet,.yards,.miles,.lightyears,.parsecs]
+    let conversions = ["Odległość","Masa","Temperatura","Czas","Objętość","Prędkość"]
+    
+//    let units: [UnitLength] = [.nanometers,.kilometers,.meters,.centimeters,.decameters,.nauticalMiles,.feet,.yards,.miles,.lightyears,.parsecs]
+    
+    
+    let unitTypes = [
+        [UnitLength.meters, UnitLength.kilometers, UnitLength.feet, UnitLength.yards, UnitLength.miles,UnitLength.parsecs,UnitLength.lightyears,UnitLength.nauticalMiles],
+        [UnitMass.grams, UnitMass.kilograms, UnitMass.ounces, UnitMass.pounds,UnitMass.carats],
+        [UnitTemperature.celsius, UnitTemperature.fahrenheit, UnitTemperature.kelvin],
+        [UnitDuration.hours, UnitDuration.minutes, UnitDuration.seconds],
+        [UnitVolume.liters, UnitVolume.bushels, UnitVolume.gallons,UnitVolume.imperialGallons,UnitVolume.pints],
+        [UnitSpeed.knots,UnitSpeed.kilometersPerHour,UnitSpeed.metersPerSecond,UnitSpeed.milesPerHour]
+    ]
+    
+    
     let formatter: MeasurementFormatter
     
     var result: String {
@@ -22,34 +39,66 @@ struct ContentView: View {
     }
     
     var body: some View {
+
         NavigationView{
+
             Form{
+
+                    
             Section{
-                TextField("Amount",value: $input, format: .number)
+                TextField("Ilość",value: $input, format: .number)
                     .keyboardType(.decimalPad)
+                    .focused($inputIsFocused)
             } header : {
-                Text("Amount to convert")
+                Text("Wartość do konwersji")
             }
             
-            Picker("Convert from",selection: $inputUnit){
-                ForEach(units,id: \.self){
-                    Text(formatter.string(from: $0).capitalized)
+                Picker("Rodzaj", selection: $selectedUnits) {
+                    ForEach(0..<conversions.count) {
+                        Text(conversions[$0])
+                    }
                 }
-            }
-            
-            Picker("Convert to",selection: $outputUnit){
-                ForEach(units,id: \.self){
-                    Text(formatter.string(from: $0).capitalized)
+
+                Picker("Konwersja z", selection: $inputUnit) {
+                    ForEach(unitTypes[selectedUnits], id: \.self) {
+                        Text(formatter.string(from: $0).capitalized)
+                    }
                 }
-            }
+
+                Picker("Konwersja na", selection: $outputUnit) {
+                    ForEach(unitTypes[selectedUnits], id: \.self) {
+                        Text(formatter.string(from: $0).capitalized)
+                    }
+                }
+                Button("Zamień"){
+                    let oldOutputUnit = outputUnit
+                    outputUnit = inputUnit
+                    inputUnit = oldOutputUnit
+                }
             Section{
                 Text(result)
             } header: {
-                Text("Result")
+                Text("Wynik")
             }
         }
-        .navigationTitle("Converter")
-    }
+      
+        .navigationTitle("Konwerter jednostek")
+        .toolbar {
+            ToolbarItemGroup(placement: .keyboard) {
+                Spacer()
+
+                Button("Dalej") {
+                    inputIsFocused = false
+                }
+            }
+       
+        }
+        .onChange(of: selectedUnits) { newSelection in
+            let units = unitTypes[newSelection]
+            inputUnit = units[0]
+            outputUnit = units[1]
+        }
+        }
 
     }
     init(){
--- UnitConverter/UnitConverterApp.swift
+++ UnitConverter/UnitConverterApp.swift
@@ -12,6 +12,7 @@ struct UnitConverterApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .preferredColorScheme(.dark)
         }
     }
 }
